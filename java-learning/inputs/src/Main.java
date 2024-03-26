13c3d7243f5cca823b2f67486669fff1a8aa0c5d
Abhishek Choudhary <abhich2109@gmail.com>
2024-03-26 16:25:22 +0530

    file for input

---
+++ java-learning/inputs/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Inputs
+        Scanner sc = new Scanner(System.in);
+        //String name = sc.next(); // Prints only one word
+        String name = sc.nextLine();
+        // nextInt
+        // nestFloat ...
+        System.out.println(name);
+    }
+}
