ec0b5ebe5402dafd592046701433f632f7f1fe47
Abhishek Choudhary <abhich2109@gmail.com>
2024-04-04 17:11:00 +0530

    added some questions

---
+++ java-learning/strings/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String fullName = "tony stark";
+        for(int i = 0; i < fullName.length(); i++)
+            System.out.println(fullName.charAt(i));
+
+        // Comparing strings
+        String name1 = "Tony";
+        String name2 = "Tony";
+
+        //1 s1>s2: +ve value
+        //2 s1==s2: 0
+        //3 s1<s2: -ve value
+        if(name1.compareTo(name2) == 0)
+            System.out.println("Strings are equal");
+        else
+            System.out.println("Strings are not equal");
+    }
+
+    private static void concatenation(){
+
+    }
+}
